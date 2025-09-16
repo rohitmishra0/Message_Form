91c2aa145361f8db151593fb874a309520f6edf8
Rohit Mishra <165405369+rohitmishra0@users.noreply.github.com>
2025-09-16 22:59:29 +0530

    Update Login Sheet.js

--- Login Sheet.js
+++ Login Sheet.js
@@ -1,5 +1,4 @@
-const scriptURL='https://script.google.com/macros/s/AKfycbyPjPWx40TJ_ynK3CYY_VosWfOkWdZPZYgUnkgMX-PmJqAR6gE0LAg_fuqW0ZVoC2L0/exec'
-
+const scriptURL='https://script.google.com/macros/s/AKfycbxtSjFn3kJ0hcqxD42tl27AS39TQM72zyo6Jt_ZpAVZ28yif5ziqt4shfDxuWw7FZnN/exec'
 const form=document.forms['login-form']
 
 form.addEventListener('submit',e=>{
@@ -8,4 +7,5 @@ form.addEventListener('submit',e=>{
     .then(response=>alert("Thank You! Your message is deliverd. Rohit will get back to you on your email."))
     .then(()=>{window.location.reload();})
     .catch(error=>console.error('Error!',error.message))
+
 })
